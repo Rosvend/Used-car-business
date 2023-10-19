6360a0a5a99d48d436fdcf31251f361cd75e0ac7
Bray Marin <bray.marin@upb.edu.co>
2023-10-19 16:35:29 -0500

    Project restarted in a Visual Studio solution

--- Program.cs
+++ Program.cs
@@ -1,27 +1,3 @@
-using System;
-using System.Collections.Generic;
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
-namespace Main
-{
-    class Program
-    {
-        static void Main()
-        {
-            List<Car> lista_de_carros = new List<Car>();
-            Car carro1 = new Car("Toyota", "AVX124", "Hilux", 1000000, 2000000, Category.OffRoad, RepairType.Mechanical, Location.Bogota);
-            lista_de_carros.Add(carro1);
-            Console.WriteLine(carro1.Model);
-
-            Car carro2 = new Car("Kia", "MIP201", "Picao", 400000, 600000, Category.Tourism, RepairType.Electrical, Location.Medellin);
-            lista_de_carros.Add(carro2);
-
-            Car carro3 = new Car("Chevrolet", "PQE387", "Twingo", 100, 500, Category.Industrial, RepairType.SheetMetal, Location.Cali);
-            lista_de_carros.Add(carro3);
-
-            foreach (Car car in lista_de_carros)
-            {
-                Console.WriteLine($"Car Model: {car.Model}");
-            }
-        }
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Used-car-business
-This is a software engineering project for a used car selling company to track their car stock, sell cars, check customers wage, and check customers information.
