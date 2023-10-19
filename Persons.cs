e5b593eb4c159e49e40d4d2ff1c8cbbc98f14b35
Rosvend <roydavidsandovalviera1@gmail.com>
2023-10-19 18:08:50 -0500

    Added new classes
    Added inheritance, and added new classes seller, buyer, potential buyer, commission seller and wage seller

--- Persons.cs
+++ Persons.cs
@@ -1,11 +1,47 @@
-﻿using System;
+using System;
+
 namespace Used_car_business
 {
-	public class Person
-	{
-		public Person()
-		{
-		}
-	}
+    public class Person
+    {
+        public Person()
+        {
+        }
+    }
+
+    public class Seller : Person
+    {
+        public Seller()
+        {
+        }
+    }
+
+    public class CommissionSeller : Seller
+    {
+        public CommissionSeller()
+        {
+        }
+    }
+
+    public class WageSeller : Seller
+    {
+        public WageSeller()
+        {
+        }
+    }
+
+    public class Buyer : Person
+    {
+        public Buyer()
+        {
+        }
+    }
+
+    public class PotentialBuyer : Person
+    {
+        public PotentialBuyer()
+        {
+        }
+    }
 }
 
