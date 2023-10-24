d84f9a4a3de3c1a5173af681a0dc83e04e42345f
Soomri <147785534+Soomri@users.noreply.github.com>
2023-10-24 08:56:19 -0500

    Add files via upload
    Versión original de Location.cs

---
+++ LocationVersionBray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Used_car_business
+
+{
+	public class Location
+	{
+		public string name { get; set; }
+		public int nit { get; set; }
+		public bool canRepair { get; set; }
+		private List<Car> cars { get; set; }
+
+        public Location(string name, int nit, bool canRepair, List<Car> cars)
+		{
+			this.name = name;
+			this.nit = nit;
+			this.canRepair = canRepair;
+			this.cars = cars;
+        }
+
+        public bool RepairCar(Car carToRepair)
+		{
+			if (canRepair)
+			{
+				if (cars.Contains(carToRepair) && (carToRepair.repairments.Count != 0 || carToRepair.repairments == null))
+				{
+                    foreach (string repairment in carToRepair.repairments)
+					{
+						Console.WriteLine($"{repairment}, reparado");
+					}
+
+					return true;
+					
+				}
+			}
+			return false;
+		}
+    }
+}
+
