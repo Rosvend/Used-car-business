649104d0b72be47f153f87aa0cc5e70345764acd
Bray Marín <bray.marin@upb.edu.co>
2023-10-24 16:15:00 -0500

    Corrected logic and atributes

--- Location.cs
+++ Location.cs
@@ -1,44 +1,64 @@
 ﻿using System;
-namespace Used_car_business
 
+namespace Used_car_business
 {
     public class Location
     {
         public string name { get; set; }
-        public int nit { get; set; }
+        public string nit { get; set; }
         public bool canRepair { get; set; }
-        private List<Car> cars { get; set; }
+        private List<Car>? cars { get; set; }
 
-        public Location(string name, int nit, bool canRepair, List<Car> cars)
+        public Location(string name, string nit, bool canRepair)
         {
             this.name = name;
             this.nit = nit;
             this.canRepair = canRepair;
-            this.cars = cars;
         }
 
         public bool RepairCar(Car carToRepair)
         {
-            if (cars.Contains(carToRepair))
+            if (cars != null)
             {
-                if (canRepair)
+                if (cars.Contains(carToRepair))
                 {
-                    foreach (string repairment in carToRepair.repairments)
+
+                    if (canRepair)
                     {
-                        Console.WriteLine($"{repairment}, reparado");
-                        carToRepair.repairments.Remove(repairment);
+                        foreach (string repairment in carToRepair.repairments)
+                        {
+                            Console.WriteLine($"{repairment}, reparado");
+                            carToRepair.repairments.Remove(repairment);
+                        }
+                        return true;
                     }
-                    return true;
+                    Console.WriteLine("Esta sede no esta habilitada para realizar reparaciones.");
+                    return false;
                 }
-                Console.WriteLine("Esta sede no esta habilitada para realizar reparaciones.");
-                return false;
+                Console.WriteLine("Este auto no se encuentra en esta sede.");
             }
-            Console.WriteLine("Este auto no se encuentra en esta sede.");
+            Console.WriteLine("No hay automoviles en esta sede.");
             return false;
+        }
+
+        public void SetCars(List<Car> cars)
+        {
+            if (cars == null)
+            {
+                cars = new() { };
+            }
+            foreach (Car car in cars)
+            {
+                cars.Add(car);
+            }
         }
 
         public List<Car> GetCars()
         {
+            if (cars == null)
+            {
+                return new() { };
+            }
             return cars;
         }
     }
