5d55046e3f3358d209143dfadce2dfc91ef5f208
Bray Marín <bray.marin@upb.edu.co>
2023-10-24 15:51:25 -0500

    Fixed Location errors and consolidated it one file

--- Location.cs
+++ Location.cs
@@ -2,38 +2,44 @@
 namespace Used_car_business
 
 {
-	public class Location
-	{
-		public string name { get; set; }
-		public int nit { get; set; }
-		public bool canRepair { get; set; }
-		private List<Car> cars { get; set; }
+    public class Location
+    {
+        public string name { get; set; }
+        public int nit { get; set; }
+        public bool canRepair { get; set; }
+        private List<Car> cars { get; set; }
 
         public Location(string name, int nit, bool canRepair, List<Car> cars)
-		{
-			this.name = name;
-			this.nit = nit;
-			this.canRepair = canRepair;
-			this.cars = cars;
+        {
+            this.name = name;
+            this.nit = nit;
+            this.canRepair = canRepair;
+            this.cars = cars;
         }
 
-public bool RepairCar(Car carToRepair)
-{
-    if (canRepair)
-    {
-        if (cars.Contains(carToRepair) && (carToRepair.repairments == null || carToRepair.repairments.Count == 0))
+        public bool RepairCar(Car carToRepair)
         {
-            Console.WriteLine("No hay reparaciones para este carro.");
+            if (cars.Contains(carToRepair))
+            {
+                if (canRepair)
+                {
+                    foreach (string repairment in carToRepair.repairments)
+                    {
+                        Console.WriteLine($"{repairment}, reparado");
+                        carToRepair.repairments.Remove(repairment);
+                    }
+                    return true;
+                }
+                Console.WriteLine("Esta sede no esta habilitada para realizar reparaciones.");
+                return false;
+            }
+            Console.WriteLine("Este auto no se encuentra en esta sede.");
             return false;
         }
 
-        foreach (string repairment in carToRepair.repairments)
+        public List<Car> GetCars()
         {
-            Console.WriteLine($"{repairment}, reparado");
+            return cars;
         }
-
-        return true;
     }
-    return false;
 }
-
