35908c191442577001b00eeee77d06a843a25f2a
Bray Marín <bray.marin@upb.edu.co>
2023-10-24 17:40:03 -0500

    Data type corrections and added the selling price get and set

--- Cars.cs
+++ Cars.cs
@@ -8,19 +8,35 @@ namespace Used_car_business
         public string brand { get; set; }
         public string model { get; set; }
         public Location office { get; set; }
-        public int sellingPrice { get; set; }
-        public List<string> repairments { get; set; }
+        public List<string>? repairments { get; set; }
 
+        private int sellingPrice { get; set; }
 
-        public Car(string brand, string model, Location office, bool isNew, int providerPrice, int sellingPrice, List<string> repairments)
+        public Car(string brand, string model, Location office, bool isNew, int providerPrice, List<string>? repairments)
         {
             this.brand = brand;
             this.model = model;
             this.office = office;
             this.isNew = isNew;
             this.providerPrice = providerPrice;
-            this.sellingPrice = sellingPrice;
             this.repairments = repairments;
+        }
+
+        public int GetSellingPrice()
+        {
+            return sellingPrice;
+        }
+
+        public void SetSellingPrice(float proffitPercentage)
+        {
+            // This method should receive percentages in the form of integers,
+            // such as "20" for 20%, instead of "0.2" for 20%
+            if (proffitPercentage >= 1)
+            {
+                sellingPrice = (int)(providerPrice + (providerPrice * (1 / proffitPercentage)));
+                return;
+            }
+            Console.WriteLine("Please enter a valid percentage as a integer. (I.E: '20' for 20%)");
         }
 
     }
@@ -29,7 +45,7 @@ namespace Used_car_business
     public class TourismCar : Car
     {
         public int passengerCapacity;
-        public TourismCar(string brand, string model, Location office, bool isNew, int providerPrice, int sellingPrice, int passengerCapacity) : base(brand, model, office, isNew, providerPrice, sellingPrice)
+        public TourismCar(string brand, string model, Location office, bool isNew, int providerPrice, int passengerCapacity, List<string> repairments) : base(brand, model, office, isNew, providerPrice, repairments)
         {
             this.passengerCapacity = passengerCapacity;
         }
@@ -38,7 +54,7 @@ namespace Used_car_business
     public class IndustrialCar : Car
     {
         public int maxium_carry_weight;
-        public IndustrialCar(string brand, string model, Location office, bool isNew, int providerPrice, int sellingPrice, int maxium_carry_weight) : base(brand, model, office, isNew, providerPrice, sellingPrice)
+        public IndustrialCar(string brand, string model, Location office, bool isNew, int providerPrice, int maxium_carry_weight, List<string> repairments) : base(brand, model, office, isNew, providerPrice, repairments)
         {
             this.maxium_carry_weight = maxium_carry_weight;
         }
@@ -47,7 +63,7 @@ namespace Used_car_business
     public class OffRoadCar : Car
     {
         public int horsePower;
-        public OffRoadCar(string brand, string model, Location office, bool isNew, int providerPrice, int sellingPrice, int horsePower) : base(brand, model, office, isNew, providerPrice, sellingPrice)
+        public OffRoadCar(string brand, string model, Location office, bool isNew, int providerPrice, int horsePower, List<string> repairments) : base(brand, model, office, isNew, providerPrice, repairments)
         {
             this.horsePower = horsePower;
         }
